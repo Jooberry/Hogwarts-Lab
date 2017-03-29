890c943a2e5df20b343bf587205ec22c50061e2c
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-29 15:33:57 +0100

    finished exercise and applied css

--- hogwarts/db/hogwarts.sql
+++ hogwarts/db/hogwarts.sql
@@ -10,6 +10,6 @@ CREATE TABLE students (
   id serial8 PRIMARY KEY,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  house VARCHAR(255),
+  house_id INT8 REFERENCES houses(id) ON DELETE CASCADE,
   age INT8
 );
--- hogwarts/db/seeds.rb
+++ hogwarts/db/seeds.rb
@@ -1,37 +1,9 @@
 require_relative('../models/student')
+require_relative('../models/house')
+require('pry-byebug')
 
-student1 = Student.new({
-  "first_name" => "Harry",
-  "last_name" => "Potter",
-  "house" => "Gryffindor",
-  "age" => 16
-  })
-
-student2 = Student.new({
-  "first_name" => "Cedric",
-  "last_name" => "Diggory",
-  "house" => "Hufflepuff",
-  "age" => 17
-  })
-
-student3 = Student.new({
-  "first_name" => "Luna",
-  "last_name" => "Lovegood",
-  "house" => "Ravenclaw",
-  "age" => 16
-  })
-
-student4 = Student.new({
-  "first_name" => "Draco",
-  "last_name" => "Malfoy",
-  "house" => "Slytherin",
-  "age" => 16
-  })
-
-student1.save()
-student2.save()
-student3.save()
-student4.save()
+House.delete_all()
+Student.delete_all()
 
 house1 = House.new({
   "name" => "Gryffindor"
@@ -50,3 +22,41 @@ house1.save()
 house2.save()
 house3.save()
 house4.save()
+
+student1 = Student.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house_id" => house1.id,
+  "age" => 16
+  })
+
+student2 = Student.new({
+  "first_name" => "Luna",
+  "last_name" => "Lovegood",
+  "house_id" => house2.id,
+  "age" => 16
+  })
+
+student3 = Student.new({
+  "first_name" => "Cedric",
+  "last_name" => "Diggory",
+  "house_id" => house3.id,
+  "age" => 17
+  })
+
+student4 = Student.new({
+  "first_name" => "Draco",
+  "last_name" => "Malfoy",
+  "house_id" => house4.id,
+  "age" => 16
+  })
+
+student1.save()
+student2.save()
+student3.save()
+student4.save()
+
+
+
+binding.pry
+nil
